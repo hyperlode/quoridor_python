36b72093f8dc4a11425c1a36295abe2b704aa2df
lode <lodeameije@gmail.com>
2018-08-27 22:56:02 -0700

    output empty board

--- board.py
+++ board.py
@@ -2,19 +2,35 @@ import wall
 
 BOARD_WIDTH = 9 
 BOARD_HEIGHT = 9 
+
+#board output (ASCII) visualization
+BOARD_CELL_EMPTY = " "
+BOARD_CELL_PLAYER_1= "1"
+BOARD_CELL_PLAYER_2= "2"
+BOARD_CELL_WALL = "O"
+BOARD_CELL_LINE_HORIZONTAL = "-"
+BOARD_CELL_LINE_VERTICAL = "|"
+BOARD_CELL_LINE_CROSSING = "+"
+
+#directions
 NORTH = lambda (x, y): (x, y-1)
 EAST  = lambda (x, y): (x+1, y)
 SOUTH = lambda (x, y): (x, y+1)
 WEST  = lambda (x, y): (x-1, y)
+
 NORTH_NORTH = lambda x:  NORTH(NORTH(x))
+SOUTH_SOUTH = lambda x:  SOUTH(SOUTH(x))
+WEST_WEST = lambda x:  WEST(WEST(x))
+EAST_EAST = lambda x:  EAST(EAST(x))
 
-# efef
-SOUTH_WEST = lambda x: SOUTH(WEST(x))
+SOUTH_WEST = lambda d: SOUTH(WEST(d))
+NORTH_WEST = lambda d: NORTH(WEST(d))
+SOUTH_EAST = lambda d: SOUTH(EAST(d))
+NORTH_EAST = lambda d: NORTH(EAST(d))
 
-
-
-ORTHO_DIRECTIONS = [NORTH, EAST, SOUTH, WEST]
-
+DIRECTIONS_ORTHO = [NORTH, EAST, SOUTH, WEST]
+DIRECTIONS_ORTHO_JUMP = [NORTH_NORTH, EAST_EAST, SOUTH_SOUTH, WEST_WEST]
+DIRECTIONS_DIAGONAL_JUMP = [NORTH_EAST, NORTH_WEST, SOUTH_EAST, SOUTH_WEST]
 
 class Board():
 
@@ -26,8 +42,6 @@ class Board():
     refer to a node with a tuple (x,y)
     '''
     
-    
-    
     def __init__(self):
         nodes = [(x,y) for x in range(BOARD_WIDTH) for y in range(BOARD_HEIGHT)]
         
@@ -37,7 +51,7 @@ class Board():
         for node in nodes:
             x,y = node
             # neighbours = [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]
-            valid_neighbours = [dir(node) for dir in ORTHO_DIRECTIONS if dir(node) in nodes]    
+            valid_neighbours = [dir(node) for dir in DIRECTIONS_ORTHO if dir(node) in nodes]    
             self.board_graph[node]["edges"] = valid_neighbours
         
         
@@ -61,20 +75,60 @@ class Board():
         #check if route free
         
         pass
+    def board_array(self):
+        #setup
+        board_array = []
+        cells_vertical = BOARD_HEIGHT * 2 + 1
+        cells_horizontal = BOARD_WIDTH * 2 + 1
+        for row in range(cells_vertical):
+            line = []
+            for col in range(cells_horizontal):
+                line.append(BOARD_CELL_EMPTY)
+            board_array.append(line)
+        
+        #lines
+        
+        
+        
+        # borders
+        for col in range(cells_horizontal):
+            for row in range(cells_vertical):
+                if row == 0 or row == cells_vertical-1 or col == 0 or col == cells_horizontal-1:
+                    board_array[row][col] = BOARD_CELL_WALL
+                elif row%2 == 0 and col%2 == 0:
+                    board_array[row][col] = BOARD_CELL_LINE_CROSSING
+                elif row%2 == 0:
+                    board_array[row][col] = BOARD_CELL_LINE_HORIZONTAL
+                elif col%2 == 0:
+                    board_array[row][col] = BOARD_CELL_LINE_VERTICAL
+                    
+                # board_array[cells_vertical - 1][col] = BOARD_CELL_LINE_HORIZONTAL
+            
+        return board_array
         
     def __str__(self):
         output = ""
-        # return str(self.board_graph)
-        for x in range(BOARD_WIDTH):
+        board = self.board_array()
+        # print(board)
+        return "\n".join(["".join(row) for row in board])
+        
+        # # return str(self.board_graph)
+        # for y in range(BOARD_HEIGHT):
+            # #wall line 
             # line = ""
-            for y in range(BOARD_HEIGHT):
-                # line += self.board_graph[(x,y)] 
-                output += "{} {} {}\n".format(x,y, str(self.board_graph[(x,y)]))
-        return output
+            # for x in range(BOARD_WIDTH):    
+                # if y==0:
+                    # #top row
+                
+                # # line += self.board_graph[(x,y)] 
+                # # output += "{} {} {}\n".format(x,y, str(self.board_graph[(x,y)]))
+                
+             # #pawn line
+        # return output
 
 if __name__ == "__main__":
     qboard = Board()
-    print(qboard)
+    print(str(qboard))
     print(SOUTH((1,2)))
     print(NORTH_NORTH((1,2)))
     
