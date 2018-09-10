b28bf7deb55b14a93378f2e5e25c02acf4261c9d
lode <lodeameije@gmail.com>
2018-09-09 22:29:57 -0700

    dijkstra test as graph

--- dijkstra_test.py
+++ dijkstra_test.py
@@ -1,44 +1,44 @@
-nodes = ('A', 'B', 'C', 'D', 'E', 'F', 'G')
-distances = {
-    'B': {'A': 5, 'D': 1, 'G': 2},
-    'A': {'B': 5, 'D': 3, 'E': 12, 'F' :5},
-    'D': {'B': 1, 'G': 1, 'E': 1, 'A': 3},
-    'G': {'B': 2, 'D': 1, 'C': 2},
-    'C': {'G': 2, 'E': 1, 'F': 16},
-    'E': {'A': 12, 'D': 1, 'C': 1, 'F': 2},
-    'F': {'A': 5, 'E': 2, 'C': 16}}
+# nodes = ('A', 'B', 'C', 'D', 'E', 'F', 'G')
+# distances = {
+    # 'B': {'A': 5, 'D': 1, 'G': 2},
+    # 'A': {'B': 5, 'D': 3, 'E': 12, 'F' :5},
+    # 'D': {'B': 1, 'G': 1, 'E': 1, 'A': 3},
+    # 'G': {'B': 2, 'D': 1, 'C': 2},
+    # 'C': {'G': 2, 'E': 1, 'F': 16},
+    # 'E': {'A': 12, 'D': 1, 'C': 1, 'F': 2},
+    # 'F': {'A': 5, 'E': 2, 'C': 16}}
 
     
-def dijkstra(graph, start_node):
-    unvisited = {node: None for node in nodes} #using None as +inf
-    visited = {}
-    current = start_node
-    currentDistance = 0
-    unvisited[current] = currentDistance
+# def dijkstra(graph, start_node):
+    # unvisited = {node: None for node in nodes} #using None as +inf
+    # visited = {}
+    # current = start_node
+    # currentDistance = 0
+    # unvisited[current] = currentDistance
 
-    while True:
-        for neighbour, distance in distances[current].items():
-            if neighbour not in unvisited: continue
-            newDistance = currentDistance + distance
-            if unvisited[neighbour] is None or unvisited[neighbour] > newDistance:
-                unvisited[neighbour] = newDistance
-        visited[current] = currentDistance
-        del unvisited[current]
-        if not unvisited: break
-        candidates = [node for node in unvisited.items() if node[1]]
-        current, currentDistance = sorted(candidates, key = lambda x: x[1])[0]
+    # while True:
+        # for neighbour, distance in distances[current].items():
+            # if neighbour not in unvisited: continue
+            # newDistance = currentDistance + distance
+            # if unvisited[neighbour] is None or unvisited[neighbour] > newDistance:
+                # unvisited[neighbour] = newDistance
+        # visited[current] = currentDistance
+        # del unvisited[current]
+        # if not unvisited: break
+        # candidates = [node for node in unvisited.items() if node[1]]
+        # current, currentDistance = sorted(candidates, key = lambda x: x[1])[0]
 
-    return visited
+    # return visited
 
 def dijkstra_graph(graph, start_node):
-    unvisited = {node: None for node in nodes} #using None as +inf
+    unvisited = {node: None for node in list(graph)} #using None as +inf
     visited = {}
     current = start_node
     currentDistance = 0
     unvisited[current] = currentDistance
 
     while True:
-        for neighbour, distance in distances[current].items():
+        for neighbour, distance in graph[current].items():
             if neighbour not in unvisited: continue
             newDistance = currentDistance + distance
             if unvisited[neighbour] is None or unvisited[neighbour] > newDistance:
@@ -51,7 +51,16 @@ def dijkstra_graph(graph, start_node):
     return visited
     
 if __name__ == "__main__":
+    nodes = ('A', 'B', 'C', 'D', 'E', 'F', 'G')
+    distances = {
+    'B': {'A': 5, 'D': 1, 'G': 2},
+    'A': {'B': 5, 'D': 3, 'E': 12, 'F' :5},
+    'D': {'B': 1, 'G': 1, 'E': 1, 'A': 3},
+    'G': {'B': 2, 'D': 1, 'C': 2},
+    'C': {'G': 2, 'E': 1, 'F': 16},
+    'E': {'A': 12, 'D': 1, 'C': 1, 'F': 2},
+    'F': {'A': 5, 'E': 2, 'C': 16}}
 
-  graph = {node:distances[node] for node in nodes}
-  print(graph)
-  print(dijkstra(graph, 'B')) # return the distances to all nodes.
+    graph = {node:distances[node] for node in nodes}
+    print(graph)
+    print(dijkstra_graph(graph, 'B')) # return the distances to all nodes.
